8a1cccd6c3f8272aabe6b645e702f679a786bb27
Jasiek <jasiek.sznajd@gmail.com>
2017-11-13 19:31:48 +0100

    kirhhoff method

--- lab2/program_lab2/nodalanalysis.py
+++ lab2/program_lab2/nodalanalysis.py
@@ -34,28 +34,90 @@ class Solver(object):
         f.close()
         self.G.add_weighted_edges_from([(self.sem[0], self.sem[1], {'r': 0, 'i': 0, 'num': edge_iter})])
 
-    def get_resistance(self):
-        R = [(r['num'],r['r']) for r in [w for u, v, w in self.G.edges.data('weight')]]
-        return R
-
     def get_current(self):
-        R = [r['r'] for r in [w for u, v, w in self.G.edges.data('weight')]]
+        R = [r['i'] for r in [w for u, v, w in self.G.edges.data('weight')]]
         return R
 
-    def solve(self):
-        print(self.G.nodes)
-        print(self.G.edges.data())
+    def sem_cycles(self):
+        i = 0
+        for cycle in nx.cycle_basis(self.G.to_undirected()):
+            if (self.sem[0] in cycle and self.sem[1] in cycle):
+                i += 1
+        return i
+
+    def solveKirchhoff(self):
+        size = (self.sem_cycles() + self.G.number_of_nodes() + 1)
+        current_row = 0
+        A = np.zeros(shape=(size, self.G.number_of_edges()))
+        B = np.zeros(size - 1)
+        for node in self.G.nodes:
+            for succ in self.G.successors(node):
+                A[node][self.G.edges[(node, succ)]['weight']['num']] = -1
+            for pred in self.G.predecessors(node):
+                A[node][self.G.edges[(pred, node)]['weight']['num']] = 1
+            current_row += 1
+        if np.count_nonzero(A[0]) == 0:
+            A = A[1:]
+            current_row += 1
+        for cycle in nx.cycle_basis(self.G.to_undirected()):
+            if (self.sem[0] in cycle and self.sem[1] in cycle):
+                for i in range(len(cycle) - 1):
+                    u = cycle[i]
+                    v = cycle[i + 1]
+                    if self.G.has_edge(u, v):
+                        A[current_row - 1][self.G.edges[(u, v)]['weight']['num']] = self.G.edges[(u, v)]['weight']['r']
+                        B[current_row - 1] = self.sem[2]
+                u = cycle[len(cycle) - 1]
+                v = cycle[0]
+                if self.G.has_edge(u, v):
+                    A[current_row - 1][self.G.edges[(u, v)]['weight']['num']] = self.G.edges[(u, v)]['weight']['r']
+                    B[current_row - 1] = self.sem[2]
+            current_row += 1
+
+        j = 0
+        for i in range(0, len(A)):
+            if (np.count_nonzero(A[i-j]) == 0 and A.shape[0] > B.shape[0]):
+                A = np.delete(A, i-j, 0)
+                j += 1
+
+        I = np.linalg.lstsq(A, B)
+        for q in [w for u, v, w in self.G.edges.data('weight')]:
+            q['i'] = abs(I[0][q['num']])
+        return I
 
     def plot(self):
-        print(self.G.edges.data())
         colors = self.get_current()
         pos = nx.circular_layout(self.G)
-        nx.draw(self.G.to_undirected(), pos, edge_color=colors, width=4, edge_cmap=plt.jet(), with_labels = True)
+        nx.draw(self.G.to_undirected(), pos, edge_color=colors, width=4, edge_cmap=plt.cm.jet, with_labels=True)
+        nx.draw_networkx_edge_labels(self.G, pos)
         plt.plot()
         plt.show()
+
+
+def generateRandomGraph(number_of_nodes, maxDegreeOutFromVertex=2, maxResistance=50):
+    G = {}
+    for u in range(number_of_nodes):
+        for j in range(random.randint(1, maxDegreeOutFromVertex)):
+            v = u
+            while (v == u or (u, v) in G or (v, u) in G):
+                v = random.randint(0, number_of_nodes - 1)
+            G[(u, v)] = random.random() * maxResistance
+    return G
+
+
+def writeToFile(G, filename):
+    a = []
+    for key in G.keys():
+        a.append([*key, G[key]])
+    a.sort()
+
+    with open(filename, 'w') as f:
+        f.write("""
+    """.join(list(map(lambda x: ' '.join(list(map(lambda el: str(el), x))), a))))
 
 
 if __name__ == "__main__":
     sol = Solver()
     sol.load()
+    print(sol.solveKirchhoff())
     sol.plot()
