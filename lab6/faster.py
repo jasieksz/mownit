51a38da85c951cde5e3d5d4efe2c34d1848ac661
Jasiek <jasiek.sznajd@gmail.com>
2018-01-20 20:07:07 +0100

    prog

--- lab6/faster.py
+++ lab6/faster.py
@@ -1,7 +1,9 @@
+import heapq
+
 import nltk
 import numpy as np
 import time
-
+import math
 from connector import dbConnector
 import spacy
 from spacy.lang.en.stop_words import STOP_WORDS
@@ -9,7 +11,10 @@ from nltk import word_tokenize
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 import re
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from scipy.sparse.linalg import svds
+from scipy.sparse import csr_matrix, lil_matrix, diags
+from sklearn.preprocessing import normalize
 
 resultpath = 'd:/db/faster'
 dbpath = 'd:/db/articles.db'
@@ -21,8 +26,10 @@ class SearchEngine:
         self.stemmer = PorterStemmer()
 
         self.rawData = self.addIdToDbData(self.getArticlesFromDb(dbpath, records))
-        self.tokens = set(np.load(resultpath + '/5k/tokens.npy')) if load else sorted(self.getTokens(self.rawData))
-        self.termDocMat = np.load(resultpath +'/5k/termDoc.npy') if load else self.getTermDocumentMatrix([self.rawData[r][2] for r in self.rawData])
+        self.tokens = set(np.load(resultpath + '/1k/tokens.npy')) if load else sorted(self.getTokens(self.rawData))
+        self.termDocMat = np.load(resultpath + '/1k/termDoc.npy') if load else self.getTermDocumentMatrix([self.rawData[r][2] for r in self.rawData])
+        self.idfTermDocMat = load_sparse_csr(resultpath + '/1k/idfTermDoc.npz') if load else self.applyIFTD(self.termDocMat)
+        self.lraIdfTermDocMat = np.load(resultpath + '/1k/lra500.npy') if load else self.applyLRA(self.idfTermDocMat, 1000)
 
     #
     # Getting aritcles from database and adding IDs
@@ -74,8 +81,9 @@ class SearchEngine:
         return text
 
     #
+    # text vector, term-document matrix, IDF
     #
-    #
+
     def getArticleVector(self, article):
         tokenDict = {t: 0 for t in self.tokens}
         article = self.getArticleTokens(article)
@@ -90,13 +98,71 @@ class SearchEngine:
         for article in data:
             vec = self.getArticleVector(article)
             a.append(vec)
-        return np.array(a).transpose()
+        return np.array(a)
+
+    def applyIFTD(self, data):
+        transformer = TfidfTransformer(use_idf=True)
+        return transformer.fit_transform(data)
 
 
+    #
+    # SVD, low rank approximation
+    #
+
+    def applyLRA(self, data, r):
+        def reduce(r1, U1, s1, V1):
+            s1 = s1[:r1]  # r first singular values
+            U1 = U1[:, :r1]
+            V1 = V1[:r1, :]
+            return U1, s1, V1
+
+        def compose(U2, s2, V2):
+            D2 = np.diag(s2)
+            return U2 @ D2 @ V2
+        U, s, V = svds(data, k=r)
+        result = compose(U, s, V)
+        return result
+
+    #
+    # correlate
+    #
+
+    def correlation(self, q, d):
+        l = q @ d
+        m = len(q) * d.shape[0]
+        return l/m
+
+
+    def getBest(self, query, k=3):
+        queryVec = np.array(self.getArticleVector(query)).reshape(-1, 1)
+        queryVec = normalize(queryVec)
+        correlations = []
+        for i in range(self.idfTermDocMat.shape[0]):
+            cor = self.correlation(queryVec, normalize(self.idfTermDocMat[i]))
+            correlations.append(cor)
+        return np.argmax(correlations), np.max(correlations)
+
+
+    def normalizeV(self, vector):
+        n = math.sqrt(sum([e**2 for e in vector]))
+        return [e / n for e in vector]
+
+def save_sparse_csr(filename,array):
+    np.savez(filename,data = array.data ,indices=array.indices,
+             indptr =array.indptr, shape=array.shape )
+
+def load_sparse_csr(filename):
+    loader = np.load(filename)
+    return csr_matrix((loader['data'], loader['indices'], loader['indptr']),
+                      shape=loader['shape'])
 
 if __name__ == '__main__':
     start = time.clock()
-    google = SearchEngine(load=True, records=5000)
+    q = 'Donald Trump Russia Election'
+    google = SearchEngine(load=True, records=1000)
+    ind, m = google.getBest(q)
+    print(google.rawData[ind][0])
     end = time.clock()
-    print(google.termDocMat.shape)
     print(end - start)
+
+
