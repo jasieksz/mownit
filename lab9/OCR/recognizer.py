3bf01c875b845bc648d41cbf4a6b81390ced9e47
jasieksz <jasiek.sznajd@gmail.com>
2018-01-24 19:52:21 +0100

    nanana

--- lab9/OCR/recognizer.py
+++ lab9/OCR/recognizer.py
@@ -10,6 +10,33 @@ class Recognizer():
         self.image = self.readImage(imagePath)
         self.fontImages = self.getLettersImages(sans)
         self.lettersPositions = {}
+        self.maxf = 63
+        self.orderAndFreq = [('x', 1),
+                     ('z', 1),
+                     ('w', 4),
+                     ('y', 4),
+                     ('j', 7),
+                     ('f', 8),
+                     ('v', 8),
+                     ('k', 9),
+                     ('g', 10),
+                     ('p', 11),
+                     ('d', 12),
+                     ('m', 13),
+                     ('b', 14),
+                     ('n', 21),
+                     ('u', 21),
+                     ('h', 26),
+                     ('a', 27),
+                     ('t', 28),
+                     ('i', 30),
+                     ('q', 32),
+                     ('r', 34),
+                     ('l', 39),
+                     ('o', 41),
+                     ('s', 45),
+                     ('e', 51),
+                     ('c', 63)]
 
     def readImage(self, path):
         result = ndimage.imread(path, mode='I')
@@ -23,7 +50,7 @@ class Recognizer():
         plt.imshow(image)
         plt.show()
 
-    def getSansPathDict(self, sans):
+    def getPathDict(self, sans):
         path = 'data/sans/' if sans else 'data/serif/'
         letters = [chr(i) for i in range(97, 123)]
         sansPathDict = {}
@@ -33,14 +60,14 @@ class Recognizer():
 
     def getLettersImages(self, sans):
         result = {}
-        paths = self.getSansPathDict(sans)
+        paths = self.getPathDict(sans)
         letters = [chr(i) for i in range(97, 123)]
-        images = [self.invertImage(self.readImage(v)) for k, v in paths]
+        images = [self.invertImage(self.readImage(v)) for k, v in paths.items()]
         for letter, i in zip(letters, range(len(letters))):
             result[letter] = images[i]
         return result
 
-    def correlateLetter(self, img, letter, color, threshold=0.82):
+    def correlateLetter(self, img, letter, color, threshold=0.83):
         fi = np.fft.fft2(self.invertImage(img))
         fp = np.fft.fft2(np.rot90(letter, 2), fi.shape)
         m = np.multiply(fi, fp)
@@ -54,6 +81,8 @@ class Recognizer():
     def getLetterPositions(self, correlation, letter):
         positions = []
         lw, lh = letter.shape
+        lw += 2
+        lh += 5
         tmpi, tmpj = (-1 * lw, -1 * lh)
         for (i, j), val in np.ndenumerate(correlation):
             if val > 0.0 and not (tmpi + lw > i and tmpj + lh > j):
@@ -61,26 +90,41 @@ class Recognizer():
                 tmpi, tmpj = i, j
         return positions
 
-    def removeLetter(self, letter, positions):
+    def removeLetter(self,letter, positions):
         for x, y in positions:
             lw, lh = self.fontImages[letter].shape
-            self.image[x - lw:x, y-lh:y] = 0
+            lh += 5
+            lw += 2
+            self.image[x - lw:x, y - lh:y] = 255
+
 
     def matchAllLetters(self):
-        for letter, letterImage in self.fontImages:
+        for element in self.orderAndFreq:
             start = time.clock()
-            correlation = self.correlateLetter(self.image, letter, 254, threshold=0.85)
-            self.lettersPositions[letter] = self.getLetterPositions(correlation, letter)
-            if (letter == 'a'):
-                self.showImage(correlation)
-                print(self.lettersPositions[letter])
+            letter = element[0]
+            letterImage = self.fontImages[letter]
+            corrCoefficient = 5 * element[1] / self.maxf / 100
+            correlation = self.correlateLetter(self.image, letterImage, 254, threshold=0.8 + corrCoefficient)
+            self.lettersPositions[letter] = self.getLetterPositions(correlation, letterImage)
             self.removeLetter(letter, self.lettersPositions[letter])
+            misc.imsave('data/results/' + letter + '.png', self.image)
             print(letter, " TIME : ", time.clock() - start)
 
 
 if __name__ == '__main__':
     start = time.clock()
-    ocery = Recognizer()
-    ocery.matchAllLetters()
-    ocery.showImage(ocery.image, 15)
+    oceery = Recognizer()
+    oceery.matchAllLetters()
+    print("TIME AFTER MATCH : ", time.clock() - start)
+    np.save('data/result_norm.npy', oceery.lettersPositions)
+
+    # dic = np.load('data/result.npy')
+    # frq = [(chr(i), dic.item()[chr(i)]) for i in range(97, 123)]
+    # print(len(dic.item()['a']))
+    # print(sorted([(e[0], len(e[1])) for e in frq], key=lambda e: e[1]))
+
+    # dic2 = np.load('data/result_norm.npy')
+    # dic2 = dic2.item()
+    # dic2 = [(k, len(v)) for k,v in dic2.items()]
+    # print(dic2)
     print(time.clock() - start)
