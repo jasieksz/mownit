b48b2c1ea2bd015cefb195c0e5a781452437b8c8
jasieksz <jasiek.sznajd@gmail.com>
2018-01-24 15:13:09 +0100

    fonts letters having fun xD

---
+++ lab9/ocr/ocr.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy import ndimage
+from scipy.misc import imresize
+import matplotlib.pyplot as plt
+import os
+from tqdm import tqdm
+import networkx as nx
+import math
+import cv2
+import string
+
